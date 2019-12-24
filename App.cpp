d2709f6073dc438d6049c610a3e8d06bee0462ce
ondralukes <mail@ondralukes.cz>
2019-12-24 22:29:08 +0000

    Added all events from BMidiRoster

--- App.cpp
+++ App.cpp
@@ -65,25 +65,52 @@ App::HandleMIDI(BMessage * message){
 	int32 producer;
 	int32 consumer;
 	message->FindInt32("be:op",&op);
-	message->FindInt32("be:id",&id);
-	message->FindString("be:type",(const char **)&type);
-	message->FindInt32("be:producer",&producer);
-	message->FindInt32("be:consumer",&consumer);
+
 	switch (op){
 		case B_MIDI_REGISTERED:
+			message->FindInt32("be:id",&id);
+			message->FindString("be:type",(const char **)&type);
 			printf("MIDI %s %d registered\n",type,id);
 			break;
 		case B_MIDI_UNREGISTERED:
+			message->FindInt32("be:id",&id);
+			message->FindString("be:type",(const char **)&type);
 			printf("MIDI %s %d unregistered\n",type,id);
 			break;
 		case B_MIDI_CONNECTED:
-			
+			message->FindInt32("be:producer",&producer);
+			message->FindInt32("be:consumer",&consumer);
 			printf("MIDI: Producer %d connected to consumer %d\n",producer,consumer);
 			break;
 		case B_MIDI_DISCONNECTED:
+			message->FindInt32("be:producer",&producer);
+			message->FindInt32("be:consumer",&consumer);
 			printf("MIDI: Producer %d disconnected from consumer %d\n",producer,consumer);
+			break;
+		case B_MIDI_CHANGED_NAME:
+			{
+			char name[64];
+			message->FindInt32("be:id",&id);
+			message->FindString("be:type",(const char **)&type);
+			message->FindString("be:name",(const char **)&name);
+			printf("MIDI: %s %d changed name to %s\n",type,id,name);
+			break;
+			}
+		case B_MIDI_CHANGED_LATENCY:
+			{
+			int64 latency;
+			message->FindInt64("be:latency",&latency);
+			printf("MIDI: %s %d changed latency to %d\n",type,id,latency);
+			break;
+			}
+		case B_MIDI_CHANGED_PROPERTIES:
+			message->FindInt32("be:id",&id);
+			message->FindString("be:type",(const char **)&type);
+			printf("MIDI %s %d changed properties\n",type,id);
+			break;
 	}
 }
+
 int main()
 {
 	std::srand(std::time(NULL));
